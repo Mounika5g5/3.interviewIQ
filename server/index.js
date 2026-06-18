3d557113adcad400bc6f1939a3b401f387906f98
Mounika5g5 <23r01a05g5@cmrithyderabad.edu.in>
2026-06-18 21:45:24 +0530

    Fix CORS missing https

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"aiinterviewplatform-livid.vercel.app",
+    origin:"https://aiinterviewplatform-livid.vercel.app",
     credentials:true
 }))
 
